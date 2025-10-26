50d90450be8b9141900e8424c353a1cd080a52ce
huyentran1011 <huyentran1011@gmail.com>
2025-10-25 22:08:20 -0500

    Update run-tests.js

--- run-tests.js
+++ run-tests.js
@@ -1,9 +1,11 @@
 import { execSync } from "child_process";
 import open from "open";
 
+
 const now = new Date();
 const timestamp = now.toISOString().replace(/[:.]/g, "-"); // e.g. 2025-10-11T23-45-10-123Z
 const reportDir = `cypress/reports/report_${timestamp}`;
+const latestDir = "cypress/reports/report_latest";
 const browser = process.env.npm_config_browser || "chrome";
 
 
@@ -16,5 +18,14 @@ try {
 
 console.log(`Open the html report from in ${reportDir}`);
 await open(`${reportDir}/index.html`);
+
+// Remove old "latest" folder if exists
+if (fs.existsSync(latestDir)) {
+  fs.rmSync(latestDir, { recursive: true, force: true });
+}
+
+// Copy new report to "latest" for Jenkins reporting
+execSync(`cp -r ${reportDir} ${latestDir}`);
+console.log(`Copied latest Mochawesome report to: ${latestDir}`);
 
 
